a00a0fe5e86e41640c9d523a86571c56a7931f60
leeroywking <leeroywking@gmail.com>
2019-06-06 18:43:38 -0700

    cleaned up the response object parsing using CODE like a coder would do

--- server.js
+++ server.js
@@ -92,18 +92,69 @@ function renderWeather(request, response) {
           replyObj.formattedLoc = loc
           replyObj.saveTime = saveTime
           saveDayToDB(replyObj)
-          response.render('pages/result', { indexObj: replyObj})
+          replyObj.saveTime = new Date(saveTime)
+          replyObj.time = new Date(replyObj.time * 1000).toLocaleDateString()
+          for (let property in replyObj) {
+            if (replyObj.hasOwnProperty(property)) {
+              let regexUnixTime = /\d{10}/
+              let regexTimeCheck =/[tT]ime/
+              if(regexUnixTime.test(replyObj[property]) && regexTimeCheck.test(property)) {
+                replyObj[property] = new Date(replyObj[property] * 1000).toLocaleTimeString()
+              }else{console.log(` ${property}: ${replyObj[property]} is not a match (is it a string?)`)}
+            }
+        }
+          response.render('pages/result', { indexObj: replyObj })
         })
         .catch(console.log)
     })
 }
 
-function saveDayToDB(eventObj){
-let fields = Object.keys(eventObj);
-let values = Object.values(eventObj).map(item => `'${item}'`);
-console.log(fields);
-console.log(values);
-let SQL1 = `INSERT INTO events (${fields}) VALUES (${values})`
-console.log(SQL1)
-client.query(SQL1)
+function saveDayToDB(eventObj) {
+  let fields = Object.keys(eventObj);
+  let values = Object.values(eventObj).map(item => `'${item}'`);
+  console.log(fields);
+  console.log(values);
+  let SQL1 = `INSERT INTO events (${fields}) VALUES (${values})`
+  console.log(SQL1)
+  client.query(SQL1)
+}
+
+
+
+function checkTimeouts(sqlInfo, sqlData) {
+  const timeouts = {
+    weather: 60 * 5 * 1000, // five minutes
+    yelp: 24 * 1000 * 60 * 60, // 24-Hours
+    movie: 30 * 1000 * 60 * 60 * 24, // 30-Days
+    event: 6 * 1000 * 60 * 60, // 6-Hours
+    trail: 7 * 1000 * 60 * 60 * 24 // 7-Days
+  };
+  // if there is data, find out how old it is.
+  if (sqlData.rowCount > 0) {
+    let ageOfResults = (Date.now() - sqlData.rows[0].created_at);
+    // For debugging only
+    console.log(sqlInfo.endpoint, ' AGE:', ageOfResults);
+    console.log(sqlInfo.endpoint, ' Timeout:', timeouts[sqlInfo.endpoint]);
+    // Compare the age of the results with the timeout value
+    // Delete the data if it is old
+    if (ageOfResults > timeouts[sqlInfo.endpoint]) {
+      let sql = `DELETE FROM ${sqlInfo.endpoint}s WHERE location_id=$1;`;
+      let values = [sqlInfo.id];
+      client.query(sql, values)
+        .then(() => { return null; })
+        .catch(error => handleError(error));
+    } else { return sqlData; }
+  }
+}
+
+const weekMaker = (startDay) => {
+  let startingDay = startDay.getDay()
+  let startOfWeek = new Date(startDay.getFullYear(), startDay.getMonth(), startDay.getDate() - startingDay)
+  let week = []
+  for (let i = 0; i < 7; i++) {
+    week.push(
+      new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + i)
+    )
+  }
+  return week;
 }
