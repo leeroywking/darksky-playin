0899b27d0fefa3740c7602e214a348dc7e9801be
adrianagraybill <adriana.graybill@gmail.com>
2019-07-06 22:40:04 -0700

    progress

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,27 @@
 'use-strict'
 
+// function triggered in header partial
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+$('button.loginButton').click( function() {
+  $('form.save').submit();
+});
+
 function hideThemChitlins(hideThese) {
   hideThese.forEach(item => {
     console.log(`hiding ${item} `)
